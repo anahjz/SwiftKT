b03026fa05ad9b6a15beac5b3f500e3d4372c8b8
anahjz <ana@smartech.ir>
2026-02-15 14:26:51 +0330

    Initial Commit

---
+++ SwiftKT/SwiftKT.swift
@@ -0,0 +1,9 @@
+//
+//  SwiftKT.swift
+//  SwiftKT
+//
+//  Created by Ana Hejazi on 11/26/1404 AP.
+//
+
+import Foundation
+
---
+++ SwiftKTTests/SwiftKTTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftKTTests.swift
+//  SwiftKTTests
+//
+//  Created by Ana Hejazi on 11/26/1404 AP.
+//
+
+import Testing
+@testable import SwiftKT
+
+struct SwiftKTTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
